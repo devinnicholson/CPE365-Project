8160ae7e26683d195ed2c042c9687cebc5543cee
Devin Nicholson <devinnicholson@pcp072322pcs.wireless.calpoly.edu>
2016-12-09 19:09:47 -0800

    tables commit

---
+++ projectPHP/index.php
@@ -0,0 +1,66 @@
+
+<head>
+<meta charset="utf-8">
+
+<title>UniRank</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<link href="/styles.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="main-nav">
+
+<ul class="nav">
+
+<li class="name">UniRank v1.0.0</li>
+
+<li><a href="#">Home</a></li>
+
+<li><a href="view.php">View</a></li>
+
+<li><a href="compare.php">Compare</a></li>
+<li><a href="search.php">Search</a></li>
+</ul>
+</div>
+<header>
+<img src="uni.png" alt="some image" class="profile-image">
+<h1 class="tag name">Learn about universities all around the world.</h1>
+</header>
+
+
+<main class="flex">
+<div class="card">
+
+<h2>Background</h2>
+
+<p>Aasdasdadada</p>
+<p>Isdfsdfsdfds</p>
+<ul>
+<li>To search for a college, click<a href="search.php"> here</a></li>
+</ul>
+</div>
+
+<div class="card">
+
+<h2>Top Colleges</h2>
+
+<p>sdfsdfsdfds</p>
+
+<ul class="top">
+<li>1</li>
+<li>2</li>
+<li>3</li>
+<li>4</li>
+<li>5</li>
+</ul>
+<p>kewl</p>
+</div>
+</main>
+<footer>
+</body>
+</html>
+
---
+++ projectPHP/search.php
@@ -0,0 +1,58 @@
+
+<head>
+<meta charset="utf-8">
+<script src="search.js"></script>
+<title>UniRank</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<link href="/styles.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="main-nav">
+
+<ul class="nav">
+
+<li class="name">UniRank v1.0.0</li>
+
+<li><a href="index.php">Home</a></li>
+
+<li><a href="view.php">View</a></li>
+
+<li><a href="compare.php">Compare</a></li>
+<li><a href="search.php">Search</a></li>
+</ul>
+</div>
+<header>
+<img src="uni.png" alt="some image" class="profile-image">
+<h1 class="tag name">Search Universities</h1>
+</header>
+
+
+<main class="flex">
+<div class="datagrid">
+    
+<section class="container">
+<input type="search" class="light-table-filter" data-table="order-table" placeholder="Search for a University">
+
+    <table class="order-table table">
+<thead><tr><th>header</th><th>header</th><th>header</th><th>header</th></tr></thead>
+<tfoot><tr><td colspan="4"><div id="paging"><ul><li><a href="#"><span>Previous</span></a></li><li><a href="#" class="active"><span>1</span></a></li><li><a href="#"><span>2</span></a></li><li><a href="#"><span>3</span></a></li><li><a href="#"><span>4</span></a></li><li><a href="#"><span>5</span></a></li><li><a href="#"><span>Next</span></a></li></ul></div></tr></tfoot>
+<tbody><tr><td>data</td><td>data</td><td>data</td><td>data</td></tr>
+<tr class="alt"><td>data</td><td>data</td><td>data</td><td>data</td></tr>
+<tr><td>data</td><td>data</td><td>data</td><td>data</td></tr>
+<tr class="alt"><td>data</td><td>data</td><td>data</td><td>data</td></tr>
+<tr><td>data</td><td>data</td><td>data</td><td>data</td></tr>
+</tbody>
+</table>
+</section>
+</div>
+</main>
+<footer>
+</body>
+</html>
+
+
